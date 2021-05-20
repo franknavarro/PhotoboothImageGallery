47bda8ec5d428474aa96fedac571c1aeb6dd705c
Frank Navarro <fnavarro7777@gmail.com>
2021-05-20 09:51:52 -0700

    Quick typescript fix for container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import { FC } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import MaterialContainer from '@material-ui/core/Container';
@@ -11,10 +12,17 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const Container: FC = ({ children }) => {
+interface ContainerProps {
+  className?: string;
+}
+
+const Container: FC<ContainerProps> = ({ children, className }) => {
   const classes = useStyles();
   return (
-    <MaterialContainer maxWidth="lg" classes={{ root: classes.container }}>
+    <MaterialContainer
+      maxWidth="lg"
+      classes={{ root: clsx(classes.container, className) }}
+    >
       {children ? children : <></>}
     </MaterialContainer>
   );
